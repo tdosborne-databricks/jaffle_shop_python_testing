1ab451e7f0cbafdcbd5fc8102b1e7fe7e0957ec6
Cody Peterson <cody.dkdc2@gmail.com>
2022-09-01 15:43:34 +0000

    whoops

--- demo_combined/models/pca_orders.py
+++ demo_combined/models/pca_orders.py
@@ -7,18 +7,24 @@ from sklearn.decomposition import PCA
 # define dbt model for this file
 def model(dbt, session):
 
+    # configure packages to install
     dbt.config(packages=["scikit-learn", "numpy", "pandas"])
 
+    # contrived: ref all orders to show in DAG, use one
     orders = dbt.ref("orders_sql").to_pandas()
     orders = dbt.ref("orders_snowpark").to_pandas()
     orders = dbt.ref("orders_pandas").to_pandas()
 
+    # prepare the data for statistical analytics
     orders = prep_data(orders)
 
+    # perform PCA
     orders_pca = pca_transform(orders)
 
+    # return the restult as the dbt model for this file
     return orders_pca
 
+# helper functions
 def prep_data(df):
 
     return df.drop("ORDER_DATE", axis=1).drop("STATUS", axis=1)
